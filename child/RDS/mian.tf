7bfb3c2abbc991824c441e2e27700f27d78dfcc6
kkukuruza <kkukuruza@ziyotek.net>
2023-03-18 22:49:04 -0400

    Created RDS module

---
+++ child/RDS/mian.tf
@@ -0,0 +1,23 @@
+resource "aws_db_subnet_group" "go_green" {
+  name       = "DB-subnet-group"
+  subnet_ids = var.subnet_ids
+}
+
+resource "aws_db_instance" "go_green_db" {
+  identifier           = "GoGreenDB"
+  allocated_storage    = 6144 
+  storage_type         = var.storage_type
+  engine               = "mysql"
+  engine_version       = "5.7.41"
+  instance_class       = var.instance_class
+  name                 = "admin"
+  username             = "admin"
+  password             = "gogreen2023"
+  db_subnet_group_name = aws_db_subnet_group.go_green.name
+  vpc_security_group_ids = [var.security_group_db_id]
+  kms_key_id           = var.kem_key_arn
+  multi_az             = true
+  publicly_accessible  = false
+  backup_retention_period = 0
+  skip_final_snapshot  = true
+}
---
+++ child/RDS/variables.tf
@@ -0,0 +1,28 @@
+variable "db_subnet_id" {
+  type = string
+}
+
+variable "storage_type" {
+  type = string
+  default = "gp2"
+}
+
+variable "instance_class" {
+  type = string
+  default = "db.t3.micro"
+}
+
+variable "security_group_db_id" {
+  type = list(string)
+  description = "A list of security group IDs to associate with the DB"
+}
+
+variable "kem_key_arn" {
+  type = string
+  description = "The KMS arn to associate with the DB"
+}
+
+variable "subnet_ids" {
+  type = list(string)
+  description = []
+}
