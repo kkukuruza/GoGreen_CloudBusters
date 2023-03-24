a209b034c62d2ed5582d23cb1dae007dbdf07c34
kkukuruza <kkukuruza@ziyotek.net>
2023-03-23 23:09:14 -0400

    ok

--- child/main.tf
+++ child/main.tf
@@ -1,3 +1,16 @@
+provider "aws" {
+  region = "us-west-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 module "IAM" {
   source = "child/IAM/"
 
