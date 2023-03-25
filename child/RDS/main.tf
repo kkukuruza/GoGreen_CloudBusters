11ba7e7db2222fdb85347c17b9dc0419dff04fcc
kkukuruza <kkukuruza@ziyotek.net>
2023-03-24 23:31:49 -0400

    update

--- child/RDS/main.tf
+++ child/RDS/main.tf
@@ -44,7 +44,7 @@ resource "aws_iam_role" "lambda_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_role_attachment" {
-  policy_arn = aws_iam_role_policy.lambda_policy.arn
+  policy_arn = aws_iam_role_policy.lambda_policy.policy
   role       = aws_iam_role.lambda_role.arn
 }
 
